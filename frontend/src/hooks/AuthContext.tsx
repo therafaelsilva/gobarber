42f58e08475ab0130c85e2efb6bac28fddb2c336
therafaelsilva <rafaelsilva.rx@gmail.com>
2020-10-19 07:14:22 -0300

    Toasts structure

--- frontend/src/hooks/AuthContext.tsx
+++ frontend/src/hooks/AuthContext.tsx
@@ -14,6 +14,7 @@ interface SignInCredentials {
 interface AuthContextData {
   user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);

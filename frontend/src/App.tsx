f60cd34acff0f993cc6c351cc363c5bc8b767513
therafaelsilva <rafaelsilva.rx@gmail.com>
2020-11-23 22:33:51 -0300

    Adding and removing Toasts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
-import ToastContainer from './components/ToastContainer';
+// import ToastContainer from './components/ToastContainer';
 import AppProvider from './hooks';
 
 const App: React.FC = () => (
